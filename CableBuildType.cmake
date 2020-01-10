4581d5b1e5175d20c73559f96920e5ad48f81a13
Vadim <v.voloshanov@pixelplex.io>
2020-01-10 18:15:47 +0300

    ECHO-1720: update evm to 1.8.0

--- CableBuildType.cmake
+++ CableBuildType.cmake
@@ -10,7 +10,7 @@ set(cable_build_type_included TRUE)
 macro(cable_set_build_type)
     if(NOT PROJECT_IS_NESTED)
         # Do this configuration only in the top project.
-        if(PROJECT_SOURCE_DIR)
+        if(NOT PROJECT_SOURCE_DIR)
             message(FATAL_ERROR "cable_set_build_type() can be used before project()")
         endif()
 
